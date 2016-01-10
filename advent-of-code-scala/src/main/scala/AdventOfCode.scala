de2ac17123f3363e74cb963965c001554e03b74b
Patrick <pmr3mh@gmail.com>
2016-01-10 12:42:55 -0600

    day 6 part 2

--- advent-of-code-scala/src/main/scala/AdventOfCode.scala
+++ advent-of-code-scala/src/main/scala/AdventOfCode.scala
@@ -52,60 +52,60 @@ object AdventOfCode {
     	(xs, ys).zipped.map((x, y) => List.range(x, y + 1)).sequence
     }
 
-    def turnOn(g: Map[Int,Set[Int]], x: Int, y: Int): Map[Int,Set[Int]] = {
-    	g + (x -> ((g getOrElse (x,Set[Int]())) + y))
-    }
+	// def turnOn(g: Map[Int,Set[Int]], x: Int, y: Int): Map[Int,Set[Int]] = {
+	// 	g + (x -> ((g getOrElse (x,Set[Int]())) + y))
+	// }
 
-    def turnOff(g: Map[Int,Set[Int]], x: Int, y: Int): Map[Int,Set[Int]] = {
-    	g + (x -> ((g getOrElse (x,Set[Int]())) - y))
-    }
+	// def turnOff(g: Map[Int,Set[Int]], x: Int, y: Int): Map[Int,Set[Int]] = {
+	// 	g + (x -> ((g getOrElse (x,Set[Int]())) - y))
+	// }
 
-	def turnOnLights(grid: Map[Int,Set[Int]], coordinates: List[List[Int]]): Map[Int,Set[Int]] = {
-		coordinates.foldLeft(grid){ case (g, (x :: y :: _)) => turnOn(g, x, y) }
-	}
+	// def turnOnLights(grid: Map[Int,Set[Int]], coordinates: List[List[Int]]): Map[Int,Set[Int]] = {
+	// 	coordinates.foldLeft(grid){ case (g, (x :: y :: _)) => turnOn(g, x, y) }
+	// }
 
-	def toggleLights(grid: Map[Int,Set[Int]], coordinates: List[List[Int]]): Map[Int,Set[Int]] = {
-		coordinates.foldLeft(grid){ case (g, (x :: y :: _)) => 
-			(g get x) match {
-				case Some(ys) => if(ys contains y){ turnOff(g, x, y) } else{ turnOn(g, x, y) }
-				case None => turnOn(g, x, y)
-			}
-		}
-	}
+	// def toggleLights(grid: Map[Int,Set[Int]], coordinates: List[List[Int]]): Map[Int,Set[Int]] = {
+	// 	coordinates.foldLeft(grid){ case (g, (x :: y :: _)) => 
+	// 		(g get x) match {
+	// 			case Some(ys) => if(ys contains y){ turnOff(g, x, y) } else{ turnOn(g, x, y) }
+	// 			case None => turnOn(g, x, y)
+	// 		}
+	// 	}
+	// }
 
-	def turnOffLights(grid: Map[Int,Set[Int]], coordinates: List[List[Int]]): Map[Int,Set[Int]] = {
-		coordinates.foldLeft(grid){ case (g, (x :: y :: _)) => turnOff(g, x, y) }
-	}
+	// def turnOffLights(grid: Map[Int,Set[Int]], coordinates: List[List[Int]]): Map[Int,Set[Int]] = {
+	// 	coordinates.foldLeft(grid){ case (g, (x :: y :: _)) => turnOff(g, x, y) }
+	// }
 
-	def numLit(filename: String): Int = {
-		var grid:Map[Int,Set[Int]] = Map()
-		val onpattern = "turn on (\\d+),(\\d+) through (\\d+),(\\d+)".r
-		val togglepattern = "toggle (\\d+),(\\d+) through (\\d+),(\\d+)".r
-		val offpattern = "turn off (\\d+),(\\d+) through (\\d+),(\\d+)".r
+	// def numLit(filename: String): Int = {
+	// 	var grid:Map[Int,Set[Int]] = Map()
+	// 	val onpattern = "turn on (\\d+),(\\d+) through (\\d+),(\\d+)".r
+	// 	val togglepattern = "toggle (\\d+),(\\d+) through (\\d+),(\\d+)".r
+	// 	val offpattern = "turn off (\\d+),(\\d+) through (\\d+),(\\d+)".r
 
-		Source.fromFile(filename).getLines().foreach{ line => 
-			grid = line match {
-				case onpattern(x1,y1,x2,y2) => turnOnLights(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)))
-				case togglepattern(x1,y1,x2,y2) => toggleLights(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)))
-				case offpattern(x1,y1,x2,y2) => turnOffLights(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)))
-			}
-		}
-		grid.values.foldLeft(0)(_ + _.size)
-	}
+	// 	Source.fromFile(filename).getLines().foreach{ line => 
+	// 		grid = line match {
+	// 			case onpattern(x1,y1,x2,y2) => turnOnLights(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)))
+	// 			case togglepattern(x1,y1,x2,y2) => toggleLights(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)))
+	// 			case offpattern(x1,y1,x2,y2) => turnOffLights(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)))
+	// 		}
+	// 	}
+	// 	grid.values.foldLeft(0)(_ + _.size)
+	// }
 
-	def turnOnLightsBetter(grid: Set[List[Int]], coordinates: Set[List[Int]]): Set[List[Int]] = {
+	def turnOnLightsSet(grid: Set[List[Int]], coordinates: Set[List[Int]]): Set[List[Int]] = {
 		grid | coordinates
 	}
 
-	def toggleLightsBetter(grid: Set[List[Int]], coordinates: Set[List[Int]]): Set[List[Int]] = {
+	def toggleLightsSet(grid: Set[List[Int]], coordinates: Set[List[Int]]): Set[List[Int]] = {
 		(grid &~ coordinates) | (coordinates &~ grid)
 	}
 
-	def turnOffLightsBetter(grid: Set[List[Int]], coordinates: Set[List[Int]]): Set[List[Int]] = {
+	def turnOffLightsSet(grid: Set[List[Int]], coordinates: Set[List[Int]]): Set[List[Int]] = {
 		grid &~ coordinates
 	}
 
-	def numLitBetter(filename: String): Int = {
+	def numLitSet(filename: String): Int = {
 		var grid:Set[List[Int]] = Set()
 		val onpattern = "turn on (\\d+),(\\d+) through (\\d+),(\\d+)".r
 		val togglepattern = "toggle (\\d+),(\\d+) through (\\d+),(\\d+)".r
@@ -113,18 +113,49 @@ object AdventOfCode {
 
 		Source.fromFile(filename).getLines().foreach{ line => 
 			grid = line match {
-				case onpattern(x1,y1,x2,y2) => turnOnLightsBetter(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)).toSet)
-				case togglepattern(x1,y1,x2,y2) => toggleLightsBetter(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)).toSet)
-				case offpattern(x1,y1,x2,y2) => turnOffLightsBetter(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)).toSet)
+				case onpattern(x1,y1,x2,y2) => turnOnLightsSet(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)).toSet)
+				case togglepattern(x1,y1,x2,y2) => toggleLightsSet(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)).toSet)
+				case offpattern(x1,y1,x2,y2) => turnOffLightsSet(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)).toSet)
 			}
 		}
 		grid.size
+	}
+
+	def turnOnLights2(grid: Map[List[Int],Int], coordinates: List[List[Int]]): Map[List[Int],Int] = {
+		coordinates.foreach(c => grid += (c -> ((grid getOrElse (c, 0)) + 1)))
+		grid
+	}
+
+	def toggleLights2(grid: Map[List[Int],Int], coordinates: List[List[Int]]): Map[List[Int],Int] = {
+		coordinates.foreach(c => grid += (c -> ((grid getOrElse (c, 0)) + 2)))
+		grid
+	}
+
+	def turnOffLights2(grid: Map[List[Int],Int], coordinates: List[List[Int]]): Map[List[Int],Int] = {
+		coordinates.foreach(c => grid += (c -> (if(!(grid contains c)) 0 else if(grid(c) == 0) 0 else grid(c) - 1)))
+		grid
+	}
+
+	def numLit2(filename: String): Int = {
+		var grid:Map[List[Int],Int] = Map()
+		val onpattern = "turn on (\\d+),(\\d+) through (\\d+),(\\d+)".r
+		val togglepattern = "toggle (\\d+),(\\d+) through (\\d+),(\\d+)".r
+		val offpattern = "turn off (\\d+),(\\d+) through (\\d+),(\\d+)".r
+
+		Source.fromFile(filename).getLines().foreach{ line => 
+			grid = line match {
+				case onpattern(x1,y1,x2,y2) => turnOnLights2(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)))
+				case togglepattern(x1,y1,x2,y2) => toggleLights2(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)))
+				case offpattern(x1,y1,x2,y2) => turnOffLights2(grid,range(List(x1.toInt,y1.toInt),List(x2.toInt,y2.toInt)))
+			}
+		}
+		grid.values.foldLeft(0)(_ + _)
 	}
 
 
   
 	def main(args: Array[String]): Unit = {
 		//println("Nice: " + numNice(args(0)))
-		println("Lit: " + numLitBetter(args(0)))
+		println("Lit: " + numLit2(args(0)))
 	}
 }
